2393a2a650e0c935ee8ab8ae000ab619bdedebda
nickcsimp <72917939+nickcsimp@users.noreply.github.com>
2022-03-10 16:47:52 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    import Images
+    import Histogram
+    import AnimatedHistogram
+
+    #Images.create_plots('input.txt', 'Images')
+    #Histogram.create_histogram('histogram.txt')
+    AnimatedHistogram.animate_histogram('histogram.txt')
+
+
+
+
+
+
+
+
+
+
+
+
+
